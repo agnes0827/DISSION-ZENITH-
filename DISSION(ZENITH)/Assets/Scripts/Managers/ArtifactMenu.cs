c65f571e86793648f3b459bf2c5d613a59de668e
agnes0827 <agnes20827@gmail.com>
2025-05-29 14:50:27 +0900

    메뉴 이동 DOTween으로 구현

--- DISSION(ZENITH)/Assets/Scripts/Managers/ArtifactMenu.cs
+++ DISSION(ZENITH)/Assets/Scripts/Managers/ArtifactMenu.cs
@@ -2,41 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using DG.Tweening; // DOTween 네임스페이스 추가
 
 public class ArtifactMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     private RectTransform rectTransform;
     private Vector2 originalPosition;
     private Vector2 targetPosition;
-    private bool isHovered = false;
+    private Tween currentTween; // Tweening
 
     void Start()
     {
         rectTransform = GetComponent<RectTransform>();
-        originalPosition = rectTransform.anchoredPosition; // ui 내부의 위치 저장
+        originalPosition = rectTransform.anchoredPosition;
         targetPosition = new Vector2(originalPosition.x, originalPosition.y - 80f);
-        // 원위치보다 80 아래로
-    }
-
-    void Update()
-    {
-        if (isHovered)
-        {
-            rectTransform.anchoredPosition = Vector2.Lerp(rectTransform.anchoredPosition, targetPosition, Time.deltaTime * 5);
-        }
-        else
-        {
-            rectTransform.anchoredPosition = Vector2.Lerp(rectTransform.anchoredPosition, originalPosition, Time.deltaTime * 5);
-        }
+        // 원위치보다 80 아래로 이동
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        isHovered = true;
+        currentTween?.Kill(); // 기존 Tween이 실행 중이면 종료
+        currentTween = rectTransform.DOAnchorPos(targetPosition, 0.3f).SetEase(Ease.OutQuad);
+        // targetPosition으로 이동하는 Tween 실행
     }
-
     public void OnPointerExit(PointerEventData eventData)
     {
-        isHovered = false;
+        currentTween?.Kill();
+        currentTween = rectTransform.DOAnchorPos(originalPosition, 0.3f).SetEase(Ease.OutQuad);
     }
 }
