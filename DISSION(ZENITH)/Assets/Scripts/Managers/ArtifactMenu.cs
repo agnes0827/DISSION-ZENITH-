433e4587afb99d5aa8afeaa43072cf038229feac
agnes0827 <agnes20827@gmail.com>
2025-05-30 15:36:41 +0900

    아티팩트 수집/메뉴 구현

--- DISSION(ZENITH)/Assets/Scripts/Managers/ArtifactMenu.cs
+++ DISSION(ZENITH)/Assets/Scripts/Managers/ArtifactMenu.cs
@@ -6,15 +6,15 @@ using DG.Tweening; // DOTween 네임스페이스 추가
 
 public class ArtifactMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    private RectTransform rectTransform;
+    private RectTransform menuPanel; // 메뉴 객체
     private Vector2 originalPosition;
     private Vector2 targetPosition;
     private Tween currentTween; // Tweening
 
     void Start()
     {
-        rectTransform = GetComponent<RectTransform>();
-        originalPosition = rectTransform.anchoredPosition;
+        menuPanel = GetComponent<RectTransform>();
+        originalPosition = menuPanel.anchoredPosition;
         targetPosition = new Vector2(originalPosition.x, originalPosition.y - 80f);
         // 원위치보다 80 아래로 이동
     }
@@ -22,12 +22,12 @@ public class ArtifactMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
     public void OnPointerEnter(PointerEventData eventData)
     {
         currentTween?.Kill(); // 기존 Tween이 실행 중이면 종료
-        currentTween = rectTransform.DOAnchorPos(targetPosition, 0.3f).SetEase(Ease.OutQuad);
+        currentTween = menuPanel.DOAnchorPos(targetPosition, 0.3f).SetEase(Ease.OutQuad);
         // targetPosition으로 이동하는 Tween 실행
     }
     public void OnPointerExit(PointerEventData eventData)
     {
         currentTween?.Kill();
-        currentTween = rectTransform.DOAnchorPos(originalPosition, 0.3f).SetEase(Ease.OutQuad);
+        currentTween = menuPanel.DOAnchorPos(originalPosition, 0.3f).SetEase(Ease.OutQuad);
     }
 }
