3c7ecf0ff02b0de644b31bd9109acb497c8f800b
agnes0827 <agnes20827@gmail.com>
2025-11-07 18:06:02 +0900

    feat: 세이브 화면 수정

--- DISSION(ZENITH)/Assets/Scripts/Save/SaveManager.cs
+++ DISSION(ZENITH)/Assets/Scripts/Save/SaveManager.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class SaveManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static SaveManager Instance;
+    public SaveSlotData[] slots = new SaveSlotData[3]; // 파일1~3
+
+    void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            InitializeSlots();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void InitializeSlots()
     {
-        
+        for (int i = 0; i < slots.Length; i++)
+            slots[i] = new SaveSlotData(); // 비어있는 슬롯 생성
+    }
+
+    public SaveSlotData GetSlot(int index)
+    {
+        if (index < 0 || index >= slots.Length) return null;
+        return slots[index];
+    }
+
+    public void SaveSlot(int index)
+    {
+        var data = slots[index];
+        data.hasSave = true;
+        data.playTimeSeconds = Mathf.FloorToInt(GameStateManager.Instance.totalPlayTime);
+        // data.faceSprite = /* 현재 플레이어 얼굴 이미지 참조 */;
     }
 }
